68962ec10aa489cec1e600bb163c65badddc8615
shimsoohyun <shimsh7@gmail.com>
2019-07-08 14:03:40 +0900

    190708 14:03

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -9,13 +9,21 @@ public class App {
   
   static Scanner keyScan;
   
+  static Lesson[] lessons = new Lesson[100];
+  static int lessonsSize = 0;
+
+  static Member[] members = new Member[100];
+  static int membersSize = 0;
+  
+  static Board[] boards = new Board[100];
+  static int boardsSize = 0;
+  
   public static void main(String[] args) {
     keyScan = new Scanner(System.in);
     
-    Lesson[] lessons = new Lesson[100];
+    
+    
 
-
-    int size = 0;
     
     while (true) {
       System.out.print("명령> ");
@@ -23,42 +31,127 @@ public class App {
       if (command.equals("quit")) {
         break;
       } else if (command.equals("/lesson/add")) {// 참이라면 add를 else면 그밖의 것 실행
-        // 수업 데이터를 저장할 메모리를 Lesson 설계도에 따라 만든다.
-        Lesson lesson = new Lesson();
-
-        // 사용자가 입력한 값을 Lesson 인스턴스의 각 변수에 저장한다.
-        lesson.no = getIntValue("번호 ?");
-        lesson.title = getStringValue("수업명? ");
-        lesson.contents = getStringValue("설명? ");
-        lesson.startDate = getDateValue("시작일? ");
-        lesson.endDate = getDateValue("종료일? ");
-        lesson.totalHours = getIntValue("총 수업시간? ");
-        lesson.dayHours = getIntValue("일 수업시간? ");
-
-        // 수업 데이터를 저장하고 있는 인스턴스의 주소를 레퍼런스 배열에 저장한다.
-        lessons[size++] = lesson;
-        
-        System.out.println("저장하였습니다.");
+        addLesson();// addLesson() 메서드 블록에 묶어 놓은 코드를 실행한다.
         
       } else if (command.equals("/lesson/list")) {
-        for (int i = 0; i < size; i++) {
-          // 레퍼런스 배열에서 한 개의 인스턴스 주소를 꺼낸다.
-          Lesson lesson = lessons[i];
-
-          // 그 인스턴스 주소로 찾아가서 인스턴스의 각 변수 값을 꺼내 출력한다.
-          System.out.printf("%s, %s, %s ~ %s, %s, %s\n", lesson.no, lesson.title, lesson.contents,
-              lesson.startDate, lesson.endDate, lesson.totalHours, lesson.dayHours);
-        }
+        listLesson();
+        
+      } else if (command.equals("/member/add")) {
+        addMember();
+        
+      } else if (command.equals("/member/list")) {
+        listMember();
+        
+      } else if (command.equals("/board/add")) {
+        addBoard();
+        
+      } else if(command.equals("/board/list")) {
+        listBoard();
+        
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
       }
     }
-
     System.out.println();
-    
-  
-  
+    System.out.println();
   }
+
+
+
+
+
+  private static void listBoard() {
+    for(int i=0; i < boardsSize; i++) {
+      Board board = boards[i];
+      System.out.printf("%s, %s, %s\n\n", 
+          board.no, board.contents, board.createdDate);
+    }
+  }
+
+
+
+
+
+  private static void addBoard() {
+    Board board = new Board();
+    
+    board.no = getIntValue("번호 ?");
+    board.contents = getStringValue("내용? ");
+    board.createdDate = new Date(System.currentTimeMillis());
+    
+    boards[boardsSize++] = board;
+    System.out.println("저장하였습니다.");
+  }
+
+
+
+
+
+  private static void listMember() {
+    for (int i=0; i < membersSize; i++){
+      Member member = members[i];
+      System.out.printf("%s, %s, %s, %s, %s, %s, %s\n\n", 
+          member.no, member.lectureName, member.email, member.pw, 
+          member.pic, member.phoneNum, member.signedUpDate);
+    }
+  }
+
+
+
+
+
+  private static void addMember() {
+    Member member = new Member();
+    
+    member.no = getIntValue("번호 ?");
+    member.lectureName = getStringValue("이름? ");
+    member.email = getStringValue("이메일? ");
+    member.pw = getStringValue("암호? ");
+    member.pic = getStringValue("사진? ");
+    member.phoneNum = getStringValue("전화? ");
+    member.signedUpDate = getDateValue("가입일? ");
+    
+    members[membersSize++] = member;
+    System.out.println("저장하였습니다.");
+  }
+
+
+
+  
+
+  private static void addLesson() {
+ // 수업 데이터를 저장할 메모리를 Lesson 설계도에 따라 만든다.
+    Lesson lesson = new Lesson();
+    // 사용자가 입력한 값을 Lesson 인스턴스의 각 변수에 저장한다.
+    lesson.no = getIntValue("번호 ?");
+    lesson.title = getStringValue("수업명? ");
+    lesson.contents = getStringValue("설명? ");
+    lesson.startDate = getDateValue("시작일? ");
+    lesson.endDate = getDateValue("종료일? ");
+    lesson.totalHours = getIntValue("총 수업시간? ");
+    lesson.dayHours = getIntValue("일 수업시간? ");
+
+    // 수업 데이터를 저장하고 있는 인스턴스의 주소를 레퍼런스 배열에 저장한다.
+    lessons[lessonsSize++] = lesson;
+    
+    System.out.println("저장하였습니다.");
+  }
+  
+  private static void listLesson() {
+    for (int i = 0; i < lessonsSize; i++) {
+      Lesson lesson = lessons[i];
+      System.out.printf("%s, %s, %s ~ %s, %s, %s\n\n", lesson.no, lesson.title, lesson.contents,
+          lesson.startDate, lesson.endDate, lesson.totalHours, lesson.dayHours);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
   
   private static String getStringValue(String msg) {
     while(true) {
