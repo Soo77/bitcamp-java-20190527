250547c067b84489d31c97a694243acabfb09c81
Soo77 <shimsh3@naver.com>
2019-07-17 14:30:46 +0900

    ch15 study

--- bitcamp-java-basic/src/main/java/ch15/Test03.java
+++ bitcamp-java-basic/src/main/java/ch15/Test03.java
@@ -7,6 +7,7 @@ class My3 {
   
   // 개발을 하다 보면 인스턴스의 현재 값을 간단히 확인하고 싶을 경우가 있다.
   // 그럴 경우 toString()을 오버라이딩 하라!
+  // 현재 클래스의 역할에 맞춰서 재정의하기
   @Override
   public String toString() {
     return "My3 [name=" + name + ", age=" + age + "]";
--- bitcamp-java-basic/src/main/java/ch15/Test05.java
+++ bitcamp-java-basic/src/main/java/ch15/Test05.java
@@ -4,7 +4,11 @@ package ch15;
 class My5 {
   String name;
   int age;
-  
+  String tel;
+  String email;
+  int gender;
+  boolean working;
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -16,10 +20,24 @@ class My5 {
     My5 other = (My5) obj;
     if (age != other.age)
       return false;
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    } else if (!email.equals(other.email))
+      return false;
+    if (gender != other.gender)
+      return false;
     if (name == null) {
       if (other.name != null)
         return false;
     } else if (!name.equals(other.name))
+      return false;
+    if (tel == null) {
+      if (other.tel != null)
+        return false;
+    } else if (!tel.equals(other.tel))
+      return false;
+    if (working != other.working)
       return false;
     return true;
   }
@@ -32,10 +50,18 @@ public class Test05 {
     My5 obj1 = new My5();
     obj1.name = "홍길동";
     obj1.age = 20;
+    obj1.tel = "1111-1111";
+    obj1.email = "hong@test.com";
+    obj1.gender = 1;
+    obj1.working = false;
     
     My5 obj2 = new My5();
     obj2.name = "홍길동";
     obj2.age = 20;
+    obj2.tel = "1111-1111";
+    obj2.email = "hong@test.com";
+    obj2.gender = 1;
+    obj2.working = false;
     
     System.out.println(obj1 == obj2);
     System.out.println(obj1.equals(obj2));
@@ -45,6 +71,8 @@ public class Test05 {
     // => 인스턴스의 내용물이 같은지 비교하도록 만들고 싶다면 equals()을 오버라이딩 하라!
     // => String와 wrapper 클래스는 equals() 오버라이딩 하였다.
     // => StringBuffer 클래스는 equals()를 오버라이딩 하지 않았다.
+    
+    // 인스턴스 주소는 다르지만 내용은 같다.
   }
 
 }
