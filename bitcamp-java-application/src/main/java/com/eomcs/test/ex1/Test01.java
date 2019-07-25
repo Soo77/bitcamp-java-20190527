92f099a0111337d8c7e99b2f16b9654fa173b89d
Soo77 <shimsh3@naver.com>
2019-07-25 09:16:39 +0900

    중간

---
+++ bitcamp-java-application/src/main/java/com/eomcs/test/ex1/Test01.java
@@ -0,0 +1,13 @@
+package com.eomcs.test.ex1;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    Calculator.plus(100);
+    Calculator.minus(30);
+    System.out.println(Calculator.result);
+    
+    
+  }
+
+}
--- bitcamp-java-basic/src/main/java/ch21/g/Test02_3.java
+++ bitcamp-java-basic/src/main/java/ch21/g/Test02_3.java
@@ -1,17 +1,13 @@
-// 예외 처리 연습 II - 예외 처리 안했을 때
+// 예외 처리 연습 II - 호출자에게 전달하기
 package ch21.g;
 
-import java.io.FileReader;
-
 public class Test02_3 {
-  public static void main(String[] args) {
+  
+  public static void main(String[] args) throws RuntimeException {
 
-    System.out.println(100/ 0);
-
-    // RuntimeException에 대해서는 컴파일러가 예외처리를 요구하지 않는다.
-    // 다만 예외가 발생하면 호출자에게 전달한다.
-    // 즉 여기서 예외가 발생하면 main()의 호출자인 JVM에게 전달한다.
-    
-    
+    System.out.println(100 / 0);
+    // RuntimeException 예외가 발생되는 코드를 작성할 경우
+    // 메서드 선언부에 굳이 throws를 포함할 필요는 없다.
+    // 그런 불편함을 없애기 위해 만든 예외가 RuntimeException이 아닌가!
   }
 }
